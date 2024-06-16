ee84ae0c709f3dff0998afd4448d1ccce0f29214
b_shk <118120342+Bshakkak@users.noreply.github.com>
2024-06-16 17:28:41 +0400

    fifth Commit

--- resources/main.js
+++ resources/main.js
@@ -9,7 +9,13 @@ const smothMenu = (state) =>{
         sideMenu.style.marginTop = '4.3125rem';
         setTimeout(()=>{
             sideMenu.querySelectorAll('li').forEach(li => li.style.display='block');
-        }, 350)
+        }, 350);
+        setTimeout(()=>{
+            sideMenu.querySelectorAll('li').forEach(li => li.onclick = ()=>{
+                sideMenu.style.marginTop = '-8.3125rem';
+                sideMenu.querySelectorAll('li').forEach(li => li.style.display='none');
+            }, 351);
+        })
      }else
      {
         sideMenu.style.marginTop = '-8.3125rem';
@@ -30,11 +36,14 @@ menu.addEventListener('click', ()=>{
     smothMenu(menuState)
 })
 
-
-document.getElementById('txtSumm').innerHTML = `A highly motivated computer scientist, electrical & computer engineer, and scientific researcher specialized in data science,
+let summText = `A highly motivated computer scientist, electrical & computer engineer, and scientific researcher specialized in data science,
 with experiences in data analysis and visualization, web development, modeling and simulation, image processing, and 
 promoting ERP systems; and with a set of skills in Python, JavaScript, MATLAB, Java, and NI Multisim. Academically I have 
 published journals on image encryption, secure ML based facial recognition systems (PCA and LDA), CNN face recognition
 and soft computing implementations on tourism sector, and submitted a research on RF-EMR emissions measurement on 
 North Al-Batinah region to TRC Oman under the supervision of Sohar University.`
+
+
+document.getElementById('txtSumm').innerHTML = summText;
+document.getElementById('txtSummTM').innerHTML = summText;
 
